e8717c80aca8e7434e4871c372ca6d5cdfeb1bcf
Jayaprakash <34165580+y2kstack@users.noreply.github.com>
2023-05-31 11:16:50 +0530

    Update race_condition.rb

--- Critical_Section_race_conditon/race_condition.rb
+++ Critical_Section_race_conditon/race_condition.rb
@@ -1,1 +1,26 @@
+$randInt = rand(1..100)
+mutex = Mutex.new
 
+printerThread = Thread.new do
+    while true
+        mutex.synchronize do
+            if $randInt % 5 == 0
+                puts ("#{$randInt} is divisible by 5")
+                if $randInt % 5 != 0
+                    puts ("#{$randInt} is not divisible by 5")
+                end
+            end
+        end
+    end
+end
+
+updaterThread = Thread.new do
+
+    while true
+        mutex.synchronize do
+            $randInt = rand(1..100)
+        end
+    end
+end
+
+sleep(20)
